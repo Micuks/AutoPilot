384e950e167b00cbd11b924133b866fb83ce3311
micuks <sdkjwql@gmail.com>
2021-08-28 17:59:13 +0800

    Added EGearMode
    Former-commit-id: 55fd3aeffad85ee1071b0a906fb3697f0d37bc30

--- Workshop/AEB/src/AEBSample.cpp
+++ Workshop/AEB/src/AEBSample.cpp
@@ -14,117 +14,133 @@
 int main()
 {
 
-	bool inAEBState = false;
-	bool isSimOneInitialized = false;
-	StartSimOne::WaitSimOneIsOk(true);
-	SimOneSM::SetDriverName(0, "AEB");
+    bool inAEBState = false;
+    bool isSimOneInitialized = false;
+    StartSimOne::WaitSimOneIsOk(true);
+    SimOneSM::SetDriverName(0, "AEB");
 
-	int timeout = 20;
-	while (true) {
-		if (SimOneSM::LoadHDMap(timeout)) {
-			SimOneAPI::bridgeLogOutput(ELogLevel_Type::ELogLevelInformation, "HDMap Information Loaded");
-			break;
-		}
-		SimOneAPI::bridgeLogOutput(ELogLevel_Type::ELogLevelInformation, "HDMap Information Loading...");
-	}
+    int timeout = 20;
+    while (true) {
+        if (SimOneSM::LoadHDMap(timeout)) {
+            SimOneAPI::bridgeLogOutput(ELogLevel_Type::ELogLevelInformation, "HDMap Information Loaded");
+            break;
+        }
+        SimOneAPI::bridgeLogOutput(ELogLevel_Type::ELogLevelInformation, "HDMap Information Loading...");
+    }
 
-	while (true) {
-		int frame = SimOneAPI::Wait();
+    while (true) {
+        int frame = SimOneAPI::Wait();
 
-		if (SimOneAPI::GetCaseRunStatus() == SimOne_Case_Status::SimOne_Case_Status_Stop) {
-			break;
-		}
+        if (SimOneAPI::GetCaseRunStatus() == SimOne_Case_Status::SimOne_Case_Status_Stop) {
+            break;
+        }
 
-		std::unique_ptr<SimOne_Data_Gps> pGps = std::make_unique<SimOne_Data_Gps>();
-		if (!SimOneAPI::GetSimOneGps(pGps.get())) {
-			SimOneAPI::bridgeLogOutput(ELogLevel_Type::ELogLevelWarning, "Fetch GPS failed");
-		}
-       
-		std::unique_ptr<SimOne_Data_Obstacle> pObstacle = std::make_unique<SimOne_Data_Obstacle>();
-		if (!SimOneAPI::GetSimOneGroundTruth(pObstacle.get())) {
-			SimOneAPI::bridgeLogOutput(ELogLevel_Type::ELogLevelWarning, "Fetch obstacle failed");
-		}
-		
-		if (SimOneAPI::GetCaseRunStatus() == SimOne_Case_Status::SimOne_Case_Status_Running && pObstacle->timestamp > 0 && pGps->timestamp > 0) {
-			if (!isSimOneInitialized) {
-				SimOneAPI::bridgeLogOutput(ELogLevel_Type::ELogLevelInformation, "SimOne Initialized!");
-				isSimOneInitialized = true;
-			}
+        std::unique_ptr<SimOne_Data_Gps> pGps = std::make_unique<SimOne_Data_Gps>();
+        if (!SimOneAPI::GetSimOneGps(pGps.get())) {
+            SimOneAPI::bridgeLogOutput(ELogLevel_Type::ELogLevelWarning, "Fetch GPS failed");
+        }
 
-			SSD::SimPoint3D mainVehiclePos(pGps->posX, pGps->posY, pGps->posZ);
-			double mainVehicleSpeed = UtilMath::calculateSpeed(pGps->velX, pGps->velY, pGps->velZ);
+        std::unique_ptr<SimOne_Data_Obstacle> pObstacle = std::make_unique<SimOne_Data_Obstacle>();
+        if (!SimOneAPI::GetSimOneGroundTruth(pObstacle.get())) {
+            SimOneAPI::bridgeLogOutput(ELogLevel_Type::ELogLevelWarning, "Fetch obstacle failed");
+        }
 
-			double minDistance = std::numeric_limits<double>::max();
-			int potentialObstacleIndex = pObstacle->obstacleSize;
-			SSD::SimString mainVehicleLaneId = SampleGetNearMostLane(mainVehiclePos);
-			SSD::SimString potentialObstacleLaneId = "";
-			for (size_t i = 0; i < pObstacle->obstacleSize; ++i) {
-				SSD::SimPoint3D obstaclePos(pObstacle->obstacle[i].posX, pObstacle->obstacle[i].posY, pObstacle->obstacle[i].posZ);
-				SSD::SimString obstacleLaneId = SampleGetNearMostLane(obstaclePos);
-				if (mainVehicleLaneId == obstacleLaneId) {
-					double obstacleDistance = UtilMath::planarDistance(mainVehiclePos, obstaclePos);
+        if (SimOneAPI::GetCaseRunStatus() == SimOne_Case_Status::SimOne_Case_Status_Running && pObstacle->timestamp > 0 && pGps->timestamp > 0) {
+            if (!isSimOneInitialized) {
+                SimOneAPI::bridgeLogOutput(ELogLevel_Type::ELogLevelInformation, "SimOne Initialized!");
+                isSimOneInitialized = true;
+            }
 
-					if (obstacleDistance < minDistance) {
-						minDistance = obstacleDistance;
-						potentialObstacleIndex = (int)i;
-						potentialObstacleLaneId = obstacleLaneId;
-					}
-				}
-			}
+            SSD::SimPoint3D mainVehiclePos(pGps->posX, pGps->posY, pGps->posZ);
+            double mainVehicleSpeed = UtilMath::calculateSpeed(pGps->velX, pGps->velY, pGps->velZ);
 
-			auto& potentialObstacle = pObstacle->obstacle[potentialObstacleIndex];
-			double obstacleSpeed = UtilMath::calculateSpeed(potentialObstacle.velX, potentialObstacle.velY, potentialObstacle.velZ);
+            double minDistance = std::numeric_limits<double>::max();
+            int potentialObstacleIndex = pObstacle->obstacleSize;
+            SSD::SimString mainVehicleLaneId = SampleGetNearMostLane(mainVehiclePos);
+            SSD::SimString potentialObstacleLaneId = "";
+            for (size_t i = 0; i < pObstacle->obstacleSize; ++i) {
+                SSD::SimPoint3D obstaclePos(pObstacle->obstacle[i].posX, pObstacle->obstacle[i].posY, pObstacle->obstacle[i].posZ);
+                SSD::SimString obstacleLaneId = SampleGetNearMostLane(obstaclePos);
+                if (mainVehicleLaneId == obstacleLaneId) {
+                    double obstacleDistance = UtilMath::planarDistance(mainVehiclePos, obstaclePos);
+
+                    if (obstacleDistance < minDistance) {
+                        minDistance = obstacleDistance;
+                        potentialObstacleIndex = (int)i;
+                        potentialObstacleLaneId = obstacleLaneId;
+                    }
+                }
+            }
+
+            auto& potentialObstacle = pObstacle->obstacle[potentialObstacleIndex];
+            double obstacleSpeed = UtilMath::calculateSpeed(potentialObstacle.velX, potentialObstacle.velY, potentialObstacle.velZ);
 
 
-			SSD::SimPoint3D potentialObstaclePos(potentialObstacle.posX, potentialObstacle.posY, potentialObstacle.posZ);
-			double sObstalce = 0;
-			double tObstacle = 0;
+            SSD::SimPoint3D potentialObstaclePos(potentialObstacle.posX, potentialObstacle.posY, potentialObstacle.posZ);
+            double sObstalce = 0;
+            double tObstacle = 0;
 
-			double sMainVehicle = 0;
-			double tMainVehicle = 0;
+            double sMainVehicle = 0;
+            double tMainVehicle = 0;
 
-			bool isObstalceBehind = false;
-			if (!potentialObstacleLaneId.Empty()) {
+            bool isObstalceBehind = false;
+            if (!potentialObstacleLaneId.Empty()) {
 
-				SampleGetLaneST(potentialObstacleLaneId, potentialObstaclePos, sObstalce, tObstacle);
-				SampleGetLaneST(mainVehicleLaneId, mainVehiclePos, sMainVehicle, tMainVehicle);
+                SampleGetLaneST(potentialObstacleLaneId, potentialObstaclePos, sObstalce, tObstacle);
+                SampleGetLaneST(mainVehicleLaneId, mainVehiclePos, sMainVehicle, tMainVehicle);
 
-				isObstalceBehind = !(sMainVehicle >= sObstalce);
-			}
+                SimOneAPI::bridgeLogOutput(ELogLevel_Type::ELogLevelDebug, "sMainVehicle = %f, sObstacle = %f", sMainVehicle, sObstalce);
 
-			std::unique_ptr<SimOne_Data_Control> pControl = std::make_unique<SimOne_Data_Control>();
+                isObstalceBehind = !(sMainVehicle >= sObstalce);
+            }
 
-			// Control mainVehicle with SimOneDriver
-			SimOneSM::GetDriverControl(0, pControl.get());
+            std::unique_ptr<SimOne_Data_Control> pControl = std::make_unique<SimOne_Data_Control>();
 
-			// Control mainVehicle without SimOneDriver
-			/*pControl->throttle = 0.5;
-			pControl->brake = 0;
-			pControl->steering = 0;
-			pControl->handbrake = 0;
-			pControl->isManualGear = 0;
-			pControl->gear = static_cast<EGearMode>(1);*/
+            // Control mainVehicle with SimOneDriver
+            SimOneSM::GetDriverControl(0, pControl.get());
 
-			if (isObstalceBehind) {
-				double defaultDistance = 10.f;
-				double timeToCollision = std::abs((minDistance - defaultDistance)) / (obstacleSpeed - mainVehicleSpeed);
-				double defautlTimeToCollision = 3.4f;
-				if (-timeToCollision < defautlTimeToCollision && timeToCollision < 0) {
-					inAEBState = true;
-					pControl->brake = (float)(mainVehicleSpeed * 3.6 * 0.65 + 0.20);
-				}
+            // Control mainVehicle without SimOneDriver
+            /*pControl->throttle = 0.5;
+              pControl->brake = 0;
+              pControl->steering = 0;
+              pControl->handbrake = 0;
+              pControl->isManualGear = 0;
+              pControl->gear = static_cast<EGearMode>(1);*/
 
-				if (inAEBState) {
-					pControl->throttle = 0.;
-				}
-			}	
-			SimOneSM::SetDrive(0, pControl.get());
-		}
-		else {
-			SimOneAPI::bridgeLogOutput(ELogLevel_Type::ELogLevelInformation, "SimOne Initializing...");
-		}
+            if (isObstalceBehind) {
+                //EGear Mode
 
-		SimOneAPI::NextFrame(frame);
-	}
-	return 0;
+                double defaultDistance = 10.f;
+
+                double timeToCollision = std::abs((minDistance - defaultDistance) / (obstacleSpeed - mainVehicleSpeed));
+                //double timeToCollision = std::abs(minDistance - defaultDistance) * 2 / mainVehicleSpeed;
+                double defautlTimeToCollision = 3.4f;
+                //				if (-timeToCollision < defautlTimeToCollision && timeToCollision < 0) {
+                //					inAEBState = true;
+                //					pControl->brake = (float)(mainVehicleSpeed * 3.6 * 0.65 + 0.20);
+                //				}
+
+                if (timeToCollision < defautlTimeToCollision && timeToCollision > 0) {
+                    inAEBState = true;
+
+                }
+                if (inAEBState) {
+                    pControl->gear = EGearMode_Drive;
+                    pControl->throttleMode = EThrottleMode_Accel;
+                    pControl->isManualGear = 0;
+                    double accel = (mainVehicleSpeed - obstacleSpeed) * (mainVehicleSpeed - obstacleSpeed) / (2 * std::abs(minDistance - defaultDistance));
+                    pControl->throttle = -accel;
+                    SimOneAPI::bridgeLogOutput(ELogLevel_Type::ELogLevelDebug, "Acceleration: %f, distance: %f", accel, std::abs(minDistance));
+                        //	pControl->throttle = 0.;
+                }
+            }	
+            SimOneSM::SetDrive(0, pControl.get());
+        }
+        else {
+            SimOneAPI::bridgeLogOutput(ELogLevel_Type::ELogLevelInformation, "SimOne Initializing...");
+        }
+
+        SimOneAPI::NextFrame(frame);
+    }
+    return 0;
 }
---
+++ build.sh
@@ -0,0 +1,8 @@
+cd ./Build
+sh rebuild_release.sh
+cd ..
+echo Successfully rebuild.
+rm AEBCpp.zip
+zip -j AEBCpp.zip ./bin/AEB
+zip -j AEBCpp.zip ./lib/Linux64/*
+echo Success!!!
